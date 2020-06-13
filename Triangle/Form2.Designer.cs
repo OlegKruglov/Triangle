998d4aaef5e50e0f86df326b12841915af3b70a8
алекс <@DESKTOP-TVQ711I>
2020-06-13 18:53:16 +0300

    Form2.1

--- Triangle/Form2.Designer.cs
+++ Triangle/Form2.Designer.cs
@@ -29,69 +29,53 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtH = new System.Windows.Forms.TextBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // listBox1
+            // txtB
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(668, 51);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
-            this.listBox1.TabIndex = 0;
+            this.txtB.Location = new System.Drawing.Point(668, 315);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(120, 20);
+            this.txtB.TabIndex = 2;
             // 
-            // pictureBox1
+            // txtC
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(7, 84);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(430, 364);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.txtC.Location = new System.Drawing.Point(668, 354);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(120, 20);
+            this.txtC.TabIndex = 3;
             // 
-            // textBox1
+            // txtA
             // 
-            this.textBox1.Location = new System.Drawing.Point(668, 215);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(120, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtA.Location = new System.Drawing.Point(668, 274);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(120, 20);
+            this.txtA.TabIndex = 4;
             // 
-            // textBox2
+            // txtH
             // 
-            this.textBox2.Location = new System.Drawing.Point(668, 258);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 20);
-            this.textBox2.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(668, 170);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(120, 20);
-            this.textBox3.TabIndex = 4;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(668, 296);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(120, 20);
-            this.textBox4.TabIndex = 5;
+            this.txtH.Location = new System.Drawing.Point(668, 392);
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(120, 20);
+            this.txtH.TabIndex = 5;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(583, 359);
+            this.radioButton1.Location = new System.Drawing.Point(443, 421);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(85, 17);
             this.radioButton1.TabIndex = 6;
@@ -102,7 +86,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(583, 392);
+            this.radioButton2.Location = new System.Drawing.Point(534, 421);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(85, 17);
             this.radioButton2.TabIndex = 7;
@@ -112,12 +96,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(449, 60);
+            this.button1.Location = new System.Drawing.Point(453, 274);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
             this.button1.Text = "Старт";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -128,21 +113,53 @@
             this.label1.TabIndex = 9;
             this.label1.Text = "Треугольник";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(430, 364);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(453, 47);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(335, 221);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Поле";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Значение";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtH);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.listBox1);
             this.Name = "Form2";
             this.Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -152,16 +169,17 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtH;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
--- Triangle/Form2.cs
+++ Triangle/Form2.cs
@@ -16,5 +16,39 @@ namespace Triangle
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, h;
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+            c = Convert.ToDouble(txtC.Text);
+            h = Convert.ToDouble(txtH.Text);
+            Triangle triangle = new Triangle(a, b, c, h);
+            listView1.Items.Add("Сторона a");
+            listView1.Items.Add("Сторона b");
+            listView1.Items.Add("Сторона c");
+            listView1.Items.Add("Высота");
+            listView1.Items.Add("Периметр");
+            listView1.Items.Add("Площадь");
+            listView1.Items.Add("Существует?");
+            listView1.Items.Add("Спецификатор");
+            listView1.Items[0].SubItems.Add(triangle.outputA());
+            listView1.Items[1].SubItems.Add(triangle.outputB());
+            listView1.Items[2].SubItems.Add(triangle.outputC());
+            listView1.Items[3].SubItems.Add(triangle.outputH());
+            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Perimeter()));
+            listView1.Items[5].SubItems.Add(Convert.ToString(triangle.Surface()));
+            if (triangle.ExistTriangle) { listView1.Items[6].SubItems.Add("Существует"); }
+            else listView1.Items[6].SubItems.Add("Не существует");
+            if (a == b && b == c && c == b) { listView1.Items[7].SubItems.Add("Равносторонний"); }
+            else if (b == c) { listView1.Items[7].SubItems.Add("Равнобедренный"); }
+            else { listView1.Items[7].SubItems.Add("Разносторонний"); }
+        }
     }
 }
